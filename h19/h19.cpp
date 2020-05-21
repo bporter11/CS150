7489f590dbad4c04fe9c801c15188c39ee28caf0
bporter11 <54635679+bporter11@users.noreply.github.com>
2020-05-21 01:54:52 -0700

    Add files via upload

---
+++ h19/h19.cpp
@@ -0,0 +1,87 @@
+/**
+ *  @author bporter11
+ *  @date 10/22/2019
+ *  @file h11.cpp
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <fstream>
+#include <cctype>
+using namespace std;
+
+string STUDENT = "bporter11"; // Add your Canvas/occ-email ID
+
+
+#include "h19.h"
+
+// Place your function here
+vector<WORD> spellCheck(istream& in, const vector<string>& dictionary, const vector<string>& excluded)
+{
+vector<WORD> result;
+string word;
+long long pos;
+while(in)
+{
+   pos = static_cast<long long>(in.tellg());
+   if(pos == -1) break;
+    in >> word >> ws;
+    string temp;
+    for(char c : word)
+    {
+        if(!ispunct(c)) temp += tolower(c);
+    }
+    word = temp;
+    bool found = false;
+    for(WORD& e : result)
+    {
+        if (word == e.word)
+        {
+        found = true;
+         e.positions.push_back(pos);
+         continue;
+        }
+    }
+  if(!found)
+  {
+      for(string ex : excluded)
+      {
+          if(word == ex)
+          {
+          found = true;
+          continue;
+          }
+      }
+  }
+  if(!found)
+ {
+     for(string d : dictionary)
+      {
+          if (word == d)
+          {
+          found = true;
+          continue;
+          }
+      }
+ }
+   if(!found)
+  {
+      WORD a;
+      a.word = word;
+      a.positions.push_back(pos);
+      result.push_back(a);
+  }
+}
+return result;
+}
+
+
+
+/////////////// STUDENT TESTING ////////////////////
+int run()
+{
+    cout << "Student testing" << endl;
+
+    return 0;
+}
---
+++ h21/h21.cpp
@@ -0,0 +1,100 @@
+/**
+ *  @author brian porter
+ *  @date 10/29/2019
+ *  @file h21.cpp
+ */
+#include <string>
+#include <iostream>
+using namespace std;
+
+string STUDENT = "bporter11"; // Add your Canvas/occ-email ID
+
+
+#include "h21.h"
+
+
+const int BPP = 4;
+void greenScreen(unsigned char * const img, int width, int height)
+{
+    unsigned char *p = img;
+    auto * const end = img + width * height * BPP;
+    while (p != end)
+    {
+        if(*(p + 1) > *p * 2 && *(p + 1) > *(p + 2) * 2)
+        {
+            *p = 0;
+            *(p + 1) = 0;
+            *(p + 2) = 0;
+            *(p + 3) = 0;
+        }
+        p += 4;
+    }
+}
+
+void composite(unsigned char * const bg, unsigned char * const fg,
+                int width, int height)
+{
+    greenScreen(fg, width, height);
+    unsigned char* dest = fg;
+    unsigned char* src = bg;
+    auto const* end = fg + width * height * 4;
+    while (dest != end)
+    {
+        if (*dest == 0 && *(dest + 1) == 0 && *(dest + 2) == 0 && *(dest + 3) == 0)
+        {
+            *dest = *src;
+            *(dest + 1) = *(src + 1);
+            *(dest + 2) = *(src + 2);
+            *(dest + 3) = *(src + 3);
+        }
+        dest += 4;
+        src += 4;
+    }
+}
+
+/////////////// STUDENT TESTING ////////////////////
+
+int run()
+{
+    // Just some samples for class
+    cout << "Load images/paris.jpg" << endl;
+    // 1. Load a jpg file using 4 bytes per pixel (bpp RGBA)
+    int width, height, bpp, channels = 4;
+    unsigned char * paris =
+        stbi_load("images/paris.jpg",       // input file
+                    &width, &height, &bpp,  // pointers (out)
+                    channels);              // channels (in)
+
+    // Now write it out in current folder as a 4-bytes-per-pixel PNG
+    if (stbi_write_png("paris.png", width, height, channels, paris,
+                    width * channels))
+        cout << "Find paris.png in current folder." << endl;
+    else
+        cout << "Couldn't write paris.png" << endl;
+
+    // IMPORTANT - free the memory
+    stbi_image_free(paris);
+
+    // 2. Load a png file using 1 byte per pixel (Gray scale)
+    cout << "Loading images/stegosuarus.png as gray scale." << endl;
+    channels = 1;
+    unsigned char * stego = stbi_load("images/stegosaurus.png",
+                                &width, &height, &bpp, channels);
+    cout << "Writing as stego-bw.bmp in current folder: ";
+    if (stbi_write_bmp("stego-bw.bmp", width, height, channels, stego))
+        cout << "Success!" << endl;
+    else cout << "Failed!" << endl;
+
+    stbi_image_free(stego);
+
+    // 3. Load a png file using 3 bytes (RGB only)
+    cout << "Load images/Vermeer-Milkmaid.png, 3 bpp." << endl;
+    channels = 3;
+    int quality = 50; // medium quality jpg
+    auto vermeer = stbi_load("images/Vermeer-Milkmaid.png", &width, &height, &bpp, channels);
+    stbi_write_jpg("vermeer.jpg", width, height, channels, vermeer, quality);
+    stbi_image_free(vermeer);
+    cout << "Saved as vermeer.jpg in current folder." << endl;
+
+    return 0;
+}
