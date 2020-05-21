04f0dcd5b959e811e3ccb9061b720158646874f6
bporter11 <54635679+bporter11@users.noreply.github.com>
2020-05-21 02:05:33 -0700

    Add files via upload

---
+++ h22/h22.cpp
@@ -0,0 +1,114 @@
+/**
+ *  @author brian porter
+ *  @date 10/29/2019
+ *  @file h22.cpp
+ */
+#include <string>
+#include <iostream>
+using namespace std;
+
+string STUDENT = "bporter11"; // Add your Canvas/occ-email ID
+
+
+#include "h22.h"
+
+void flip(UC* const img, int width, int height, Direction dir)
+{
+    if (dir == Direction::LtoR || dir == Direction::RtoL)
+    {
+        for (int row = 0; row <= height - 1; row++)
+        {
+            Pixel * front;
+            front = reinterpret_cast<Pixel *>(img) + row * width;
+            auto back = front + width - 1;
+
+            while (front < back)
+            {
+                auto temp = *front;
+                *front = *back;
+                *back = temp;
+                front++;
+                back--;
+            }
+        }
+    }
+    else if (dir == Direction::TtoB || dir == Direction::BtoT)
+    {
+        for (int col = 0; col <= width - 1; col++)
+        {
+            Pixel * top;
+            top = reinterpret_cast<Pixel *>(img) + col;
+            auto bottom = top + width * (height - 1);
+
+            while (top < bottom)
+            {
+                auto temp = *top;
+                *top = *bottom;
+                *bottom = temp;
+                top += width;
+                bottom -= width;
+            }
+        }
+    }
+}
+
+void mirror(UC* const img, int width, int height, Direction dir)
+{
+    if (dir == Direction::LtoR || dir == Direction::RtoL)
+    {
+        for (int row = 0; row <= height - 1; row++)
+        {
+            Pixel * front;
+            front = reinterpret_cast<Pixel *>(img) + row * width;
+            auto back = front + width - 1;
+
+            while (front < back)
+            {
+                if (dir == Direction::LtoR)
+                {
+                    *back = *front;
+                }
+                else if (dir == Direction::RtoL)
+                {
+                    *front = *back;
+                }
+                front++;
+                back--;
+            }
+        }
+    }
+    else if (dir == Direction::TtoB || dir == Direction::BtoT)
+    {
+        for (int col = 0; col <= width - 1; col++)
+        {
+            Pixel * top;
+            top = reinterpret_cast<Pixel *>(img) + col;
+            auto bottom = top + width * (height -1);
+
+            while (top < bottom)
+            {
+                if ( dir == Direction::TtoB)
+                {
+                    *bottom = *top;
+                }
+                else if (dir == Direction::BtoT)
+                {
+                    *top = *bottom;
+                }
+                top += width;
+                bottom -= width;
+            }
+        }
+    }
+}
+
+
+/////////////// STUDENT TESTING ////////////////////
+
+int run()
+{
+    // Just some samples for class
+    cout << "Try some code on your own" << endl;
+
+    return 0;
+}
---
+++ h24/h24.cpp
@@ -0,0 +1,57 @@
+/**
+ *  @author brian porter
+ *  @date 10/31/2019
+ *  @file h24.cpp
+ */
+#include <string>
+#include <iostream>
+using namespace std;
+
+string STUDENT = "bporter11"; // Add your Canvas/occ-email ID
+
+#include "h24.h"
+
+// Add your code here
+void translate(unsigned char * const img, int width, int height, int dx, int dy)
+{
+    Pixel * pixel = reinterpret_cast<Pixel *>(img);
+    for (int row = 0; row < height ; row++)
+    {
+        if(dx < 0) dx = width - abs(dx) % width;
+        else dx = dx % width;
+        for (int j = 0; j < dx; j++)
+        {
+            auto r = pixel + (row * width);
+            auto temp = r[width-1];
+            for (int i = width - 1 ; i > 0; i--)
+            {
+                r[i] = r[i-1];
+            }
+            r[0] = temp;
+        }
+    }
+        if (dy < 0) dy = height-abs(dy) % height;
+        else dy = dy % height;
+        for (int i = 0; i < dy; i++)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                auto r = pixel + width * (height -1);
+                auto lastRow = r[col];
+                for (int row = height - 1; row > 0; row--)
+                pixel[(row * width) + col] = pixel[(row - 1) * width + col];
+                pixel[col] = lastRow;
+            }
+        }
+}
+
+
+
+/////////////// STUDENT TESTING ////////////////////
+int run()
+{
+    // Just some samples for class
+    cout << "Try some code on your own" << endl;
+
+    return 0;
+}
